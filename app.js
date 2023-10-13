07ad6256bf6ea9f9c1ae33a2125ab49aebf4e3d7
Kevin Moreira <kvmoreira94@gmail.com>
2023-10-12 19:14:09 -0500

    actividad 6

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ const formMedicos=document.getElementById("text")
 const formPacientes=document.getElementById("form-Pacientes")
 
 class usuario{
-    constructor(nombre,apellido,cedula,consultorio,correo,especialidad,telefono){
+    constructor(nombre,apellido,cedula,correo,especialidad){
         this.nombre=nombre
         this.apellido=apellido
         this.cedula=cedula
-        this.telefono=telefono
-        this.consultorio=consultorio
         this.correo=correo
         this.especialidad=especialidad
     }
@@ -49,9 +47,41 @@ const mostrarMedicos=function(){
         
         cuerpoTabla.appendChild(fila)
     })
-}
+};
+const mostrarPacientes=function(){
+    let pacientes=[]
+    let cuerpoTabla=document.getElementById("tablaPaciente")
+    let localpacientes=localStorage.getItem("pacientes")
+    if(localpacientes){
+        pacientes=JSON.parse(localpacientes)
+    }
+    pacientes.forEach(paciente=> {
+        let fila=document.createElement("tr")
+        let celdaNombres=fila.insertCell()
+        let celdaApellido=fila.insertCell()
+        let celdaCedula=fila.insertCell()
+        let celdaEdad=fila.insertCell()
+        let celdaTelefono=fila.insertCell()
+        let celdaCorreo=fila.insertCell()
+        let celdaEspecialidad=fila.insertCell()
+        let celdaMedico=fila.insertCell()
+
+        celdaNombres.textContent=paciente.nombre
+        celdaApellido.textContent=paciente.apellido
+        celdaCedula.textContent=paciente.cedula
+        celdaEdad.textContent=paciente.consultorio
+        celdaTelefono.textContent=paciente.telefono
+        celdaCorreo.textContent=paciente.correo
+        celdaEspecialidad.textContent=paciente.especialidad
+        celdaMedico.textContent="sin asignar"
+        
+        cuerpoTabla.appendChild(fila)
+    })}
 if(window.location.href.endsWith("tablamedico.html")){
     mostrarMedicos()
+}
+if(window.location.href.endsWith("tablapaciente.html")){
+    mostrarPacientes()
 }
 if(window.location.href.endsWith("registroMedicos.html")){
     formMedicos.addEventListener("submit",function(event){
@@ -64,7 +94,9 @@ if(window.location.href.endsWith("registroMedicos.html")){
         let valorEspecialidad=especialidad.value
         let valorTelefono=telefono.value
     
-        const medico=new usuario(valorNombres,valorApellido,valorCedula,valorConsultorio,valorCorreo,valorEspecialidad,valorTelefono)
+        const medico=new usuario(valorNombres,valorApellido,valorCedula,valorCorreo,valorEspecialidad)
+        medico.consultorio=valorConsultorio
+        medico.telefono=valorTelefono
         let medicos=[]
         
         let localmedicos=localStorage.getItem("medicos")
@@ -77,4 +109,35 @@ if(window.location.href.endsWith("registroMedicos.html")){
     })   
 }
 
+if(window.location.href.endsWith("registropacientes.html")){
+    formPacientes.addEventListener("submit",function(event){
+        event.preventDefault()
+        let valorNombres=nombre.value
+        let valorApellido=apellido.value
+        let valorCedula=cedula.value
+        let valorEdad=edad.value
+        let valorCorreo=correo.value
+        let valorEspecialidad=especialidad.value
+    
+        const paciente=new usuario(
+            valorNombres,
+            valorApellido,
+            valorCedula,
+            valorCorreo,
+            valorEspecialidad)
 
+        paciente.edad=valorEdad
+
+        let pacientes=[]
+        
+        let localpacientes=localStorage.getItem("pacientes")
+        if(localpacientes){
+            pacientes=JSON.parse(localpacientes);
+        }
+        pacientes.push(paciente)
+
+        localStorage.setItem("pacientes",JSON.stringify(pacientes))
+
+        alert("paciente registrado");
+    })   
+}
